9120e7e3fd9b3a33b332ab119f6c4dbb12f950e0
Renan Lauriano <rlara@wiley.com>
2022-11-07 17:08:51 -0300

    Included .env file

---
+++ tests/Unit/ProductControllerTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ProductControllerTest extends TestCase
+{
+
+    private $productController;
+    public static function setUpBeforeClass() {
+        parent::setupBeforeClass();
+
+        $productService = $this->getMock
+
+    };
+
+    
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
