53f0e9abd0368d58631dafe54cbc3968baa182bb
Renan Lauriano <rlara@wiley.com>
2022-11-08 18:18:19 -0300

    Implemented CalculatorService

---
+++ app/Services/CalculatorService.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Services;
+
+class CalculatorService
+{
+    public function sum($number1, $number2){
+        return $number1 + $number2;
+    }
+}
---
+++ tests/Unit/Services/CalculatorServiceTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Services;
+
+use App\Services\CalculatorService;
+use PHPUnit\Framework\TestCase;
+
+class CalculatorServiceTest extends TestCase
+{
+
+    public function testSum()
+    {
+
+    }
+}
